e14a559720b93c6f6ce6bde7de32817e07944700
MichiKaneko <michihito1005@gmail.com>
2022-02-28 12:15:44 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   attr_accessor :remember_token
   before_save {email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,15 +9,15 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
-    # 渡された文字列のハッシュ値を返す
+  # 渡された文字列のハッシュ値を返す
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-    # ランダムなトークンを返す
+  # ランダムなトークンを返す
   def self.new_token
     SecureRandom.urlsafe_base64
   end
@@ -26,13 +27,14 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-    # 渡されたトークンがダイジェストと一致したらtrueを返す
+  
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
-    # ユーザーのログイン情報を破棄する
+  # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
